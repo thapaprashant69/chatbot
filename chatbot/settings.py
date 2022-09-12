9e6a584ae34526a07af9914072b6327d6a89eafb
Prashant Thapa <thapaprashant69@gmail.com>
2022-09-12 16:18:44 +0545

    Another bug fix

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import django_heroku
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
